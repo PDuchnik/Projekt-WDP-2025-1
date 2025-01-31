5e6d9fe17ce2d0e5107e5db6307689d5a2ceaba1
Maciej Bogusz <maciekv3rb@gmail.com>
2025-01-31 20:39:36 +0100

    Dodałem słownik śledzący ekwipunek i funkcje pozwalające na interakcję z nim

--- main.py
+++ main.py
@@ -15,6 +15,37 @@ StatsDict = {
     "Obecna Lokacja": Locations[0]
 }
 
+ItemsDict = {
+    "Karabin": {"Usable": 1, "Quantity": 0},
+    "Siekiera": {"Usable": 0, "Quantity": 0},
+    "Nóż": {"Usable": 0, "Quantity": 1},
+    "Rozpałka": {"Usable": 1, "Quantity": 2},
+    "Drewno": {"Usable": 0, "Quantity": 5},
+    "Porcja Jedzenia": {"Usable": 1, "Quantity": 3}
+}
+
+def ListEquipment(): #funkcja wypisuje wszystkie przedmioty, które gracz aktualnie posiada
+    print("Posiadane przedmioty:")
+    for item in ItemsDict.keys():
+        if(ItemsDict[item]["Quantity"]>0):
+            print(str(ItemsDict[item]["Quantity"])+"x", item)
+
+def ItemFunction(Item): #funkcja będzie wykonywać odpowiednią akcję zależnie od wybranego przedmiotu
+
+def UseItem(ItemName): #funkcja pozwala graczowi wybrać jaki z przedmiotów w ekwipunku użyć
+    if(ItemName in ItemsDict.keys() and ItemsDict[ItemName]["Usable"] == 1 and ItemsDict[ItemName]["Quantity"] > 0):
+        ItemFunction(ItemName)
+    elif(ItemName in ItemsDict.keys() and (ItemsDict[ItemName]["Usable"] == 0 or ItemsDict[ItemName]["Quantity"] < 1)):
+        print("Wybranego przedmiotu nie można teraz użyć.")
+        UseItem(0)
+    elif(ItemName == 0):
+        ListEquipment()
+        ChosenItem = str(input("Wpisz nazwę przedmiotu, który chcesz użyć."))
+        UseItem(ChosenItem)
+    else:
+        print("Wybrany przedmiot nie istnieje.")
+        UseItem(0)
+
 def Scavenge(time = 1):
     ...
     print(StatsDict["Obecna Lokacja"].Loot[0])
@@ -32,8 +63,7 @@ def StatsLog(): #funkcja wypisujaca statystyki gracza do konsoli
 
 def DisplayStartScreen(): #funkcja wywolywana na poczatku programu, wypisujaca wiadomosc startowa
     ...                   #czymkolwiek by nie byla
-def UseItem():
-    ...
+
 def MoveLocation(newLocationIndex):
     StatsDict["Obecna Lokacja"] = Locations[newLocationIndex]
     PassTime(60)
@@ -45,9 +75,7 @@ while True:
         case '1':
             StatsLog()
         case '2':
-            ...
-            #LogEquipment()
-            UseItem()
+            UseItem(0)
         case '3':
             newLocationIndex = int(input())
             MoveLocation(newLocationIndex)
